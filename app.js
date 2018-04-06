e181df101127231c947b69977041c47f6a0fba9b
GoldbeardSea <timbusch1@gmail.com>
2018-04-06 14:28:41 -0700

    Test passed, time to push

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
   var sumOne = (a + b);
-  return [ 'The sum of' + a + ' and ' + b + '' ];
+  return [ 'The sum of ' + a + ' and ' + b + ' is ' + sumOne + '.' ];
 }
 
 // Here is the test for sum(); uncomment it to run it
